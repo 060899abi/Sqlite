5a8a5832ec93e7053c2cbb0ebc627af02c195e4c
060899abi <abinaya060899@gmail.com>
2020-04-04 14:14:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/User.java
@@ -0,0 +1,18 @@
+package com.example.sqlite;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+@Entity
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "phone_number")
+    public String phone_number;
+}
